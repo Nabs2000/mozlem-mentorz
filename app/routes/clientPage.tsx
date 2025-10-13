c1eb368c9edf650b7b4a1b926671335a938940f0
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-10-13 15:16:00 -0700

    Render referral form

--- app/routes/clientPage.tsx
+++ app/routes/clientPage.tsx
@@ -1,4 +1,7 @@
+import ReferralForm from "~/components/referralForm";
 import type { Route } from "./+types/clientPage";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import type { Client } from "~/types/client.types";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,6 +10,24 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function ClientPage() {
-    return <div>Client Page</div>;
+export async function loader({ params }: Route.LoaderArgs) {
+  const userId = params.clientId;
+  const db = getFirestore();
+  const docRef = doc(db, "clients", userId);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    return {
+      id: docSnap.id,
+      ...docSnap.data(),
+    };
+  } else {
+    console.log("No such document!");
+  }
+}
+
+export default function ClientPage({ loaderData }: { loaderData: Client }) {
+  return <div>
+    Hello {loaderData.firstName}!
+    <ReferralForm client={loaderData} />
+  </div>;
 }
