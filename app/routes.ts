ee28512d315191b6393a36b6d9bcced951073f07
Nabeel Sabzwari <nabeelsabz101@gmail.com>
2025-10-14 10:57:42 -0700

    Update routes to be client/professional specific

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/login.tsx"),
-  route("user:professionalId", "routes/professionalPage.tsx"),
-  route("user:clientId", "routes/clientPage.tsx"),
+  route("professional/:professionalId", "routes/professionalPage.tsx"),
+  route("client/:clientId", "routes/clientPage.tsx"),
   route("register", "routes/register.tsx"),
 ] satisfies RouteConfig;
